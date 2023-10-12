470cdbeb2c6261abf9bc3447f63fb8b785fd506a
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-12 17:08:01 +0300

    Add project files.

---
+++ Homework2-ConsoleWithLib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            if (name == String.Empty)
+                name = "No name";
+            Console.WriteLine(Concatenationlib.Concatenate(name));
+        }
+    }
+}
